5f06196324c99bd8fa3f3567bc460e983419e30e
Philip <jhasondeleon@gmail.com>
2020-02-13 18:12:43 -0500

    [Philip] Hydro Class created

---
+++ C0778584_W2020_MID_TERM_MAD3004/C0778584_W2020_MID_TERM_MAD3004/Hydro.swift
@@ -0,0 +1,23 @@
+//
+//  Hydro.swift
+//  C0778584_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+
+class Hydro: Bill
+{
+    var agencyName: String
+    var unitConsumed: Int
+    
+    init(billId: Int, billDate: Date, billType: String, totalBillAmount: Double, agencyName: String, unitConsumed: Int) {
+        self.agencyName = agencyName
+        self.unitConsumed = unitConsumed
+        super.init(billId: billId, billDate: billDate, billType: billType, totalBillAmount: totalBillAmount)
+    }
+    
+}
