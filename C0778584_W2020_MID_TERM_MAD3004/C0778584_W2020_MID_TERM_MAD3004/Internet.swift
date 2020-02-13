4463140ef199e3c142ca92203a7cdda37dad3237
Philip <jhasondeleon@gmail.com>
2020-02-13 18:04:46 -0500

    [Philip] Internet Class created

---
+++ C0778584_W2020_MID_TERM_MAD3004/C0778584_W2020_MID_TERM_MAD3004/Internet.swift
@@ -0,0 +1,22 @@
+//
+//  Internet.swift
+//  C0778584_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Internet: Bill
+{
+    var providerName: String
+    var internetGbUsed: Int
+    
+     init(billId: Int, billDate: Date, billType: String, totalBillAmount: Double, providerName: String, internetGbUsed: Int) {
+        self.providerName = providerName
+        self.internetGbUsed = internetGbUsed
+        super.init(billId: billId, billDate: billDate, billType: billType, totalBillAmount: totalBillAmount)
+        
+    }
+}
