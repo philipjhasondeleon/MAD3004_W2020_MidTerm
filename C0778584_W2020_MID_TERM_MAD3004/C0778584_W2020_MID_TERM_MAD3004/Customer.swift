cf8afb111414aa83a6c6ec235986640e7b6825cd
Philip <jhasondeleon@gmail.com>
2020-02-13 18:16:43 -0500

    [Philip] DisplayDelegate class added

---
+++ C0778584_W2020_MID_TERM_MAD3004/C0778584_W2020_MID_TERM_MAD3004/Customer.swift
@@ -0,0 +1,20 @@
+//
+//  Customer.swift
+//  C0778584_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Customer
+{
+    var customerId: Int
+    var firstName: String
+    var lastName: String
+    var fullName: String
+    var emailId: String
+    var billDictionary: String
+    
+}
---
+++ C0778584_W2020_MID_TERM_MAD3004/C0778584_W2020_MID_TERM_MAD3004/DisplayDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  DisplayDelegate.swift
+//  C0778584_W2020_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2020-02-13.
+//  Copyright © 2020 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+protocol DisplayDelegate
+{
+    func Display()
+}
